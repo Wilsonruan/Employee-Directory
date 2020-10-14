c80379f93434127ea819696f0371fa1df0cf5660
Wilson Ruan <wilsonruan23@gmail.com>
2020-10-13 20:22:33 -0400

    Inits employee lsit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import HelloWorld from './HelloWorld';
+import React, {Component} from 'react';
+import SeachBox from './SearchBox';
+import employeeList from './employeeList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <HelloWorld/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employees: [
+        {name: "Dwight Schrute", email: "Dwight_Schrute@office.ca" },
+        {name: "John Krasinski", email: "John_Krasinski@office.ca" },
+        {name: "Jenna Fischer", email: "Jenna_Fischer@office.ca" },
+        {name: "Leslie David Baker", email: "Leslie_David_Baker@office.ca" },
+        {name: "Brian Baumgartner", email: "Brian_Baumgartner@office.ca" },
+        {name: "Angela Kinsey", email: "Angela_Kinsey@office.ca" },
+        {name: "Phyllis Smith", email: "Phyllis_Smith@office.ca" },
+        {name: "Kate Flannery", email: "Kate_Flannery@office.ca" },
+        {name: "Creed Bratton", email: "Creed_Bratton@office.ca" },
+        {name: "Oscar Nuñez", email: "Oscar_Nuñez@office.ca" },
+        {name: "B.J. Novak", email: "B.J._Novak@office.ca" },
+        {name: "Mindy Kaling", email: "Mindy_Kaling@office.ca" },
+        {name: "Ed Helms", email: "Ed_Helms@office.ca" }
+      ],
+      serachEmployee: ''
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Employee Directory</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
